7261f71ab9e092e11730c3086221f43ba2617fbd
Lucivando Sousa <lucivandosousa@gmail.com>
2024-09-20 12:14:27 -0300

    feat: create menu and view projetos

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -4,6 +4,22 @@
       <h1>Tracker</h1>
       <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="#ededed"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-stack-3 ml-3"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 2l-8 4l8 4l8 -4l-8 -4" /><path d="M4 10l8 4l8 -4" /><path d="M4 18l8 4l8 -4" /><path d="M4 14l8 4l8 -4" /></svg>
     </div>
+    <nav class="panel mt-5">
+      <ul>
+        <li><RouterLink to="/">
+          <div class="is-flex is-align-content-center">
+            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-list-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3.5 5.5l1.5 1.5l2.5 -2.5" /><path d="M3.5 11.5l1.5 1.5l2.5 -2.5" /><path d="M3.5 17.5l1.5 1.5l2.5 -2.5" /><path d="M11 6l9 0" /><path d="M11 12l9 0" /><path d="M11 18l9 0" /></svg>
+            <span class="ml-2">Tarefas</span>
+          </div>
+        </RouterLink></li>
+        <li><RouterLink to="/projetos">
+          <div class="is-flex is-align-content-center">
+            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-checklist"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9.615 20h-2.615a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8" /><path d="M14 19l2 2l4 -4" /><path d="M9 8h4" /><path d="M9 12h2" /></svg>
+            <span class="ml-2">Projetos</span>
+          </div>
+        </RouterLink></li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -23,6 +39,19 @@
   header h1 {
     color: #ededed;
     font-size: 32px;
+  }
+
+  .panel li {
+    margin: 8px 0;
+  }
+
+  .panel a {
+    color: #ededed;
+  }
+
+  .router-link-active.router-link-exact-active {
+    color: #a6d49c;
+    font-weight: bold;
   }
 
   @media only screen and (max-width: 768px) {
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,19 @@
+import Projetos from "@/views/Projetos.vue"
 import Tarefas from "@/views/Tarefas.vue"
 import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router"
 
-const rotas: RouteRecordRaw[] = [{
-  path: "/",
-  name: "Tarefas",
-  component: Tarefas
-}]
+const rotas: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "Tarefas",
+    component: Tarefas  
+  },
+  {
+    path: "/projetos",
+    name: "Projetos",
+    component: Projetos
+  }
+]
 
 const roteador = createRouter({
   history: createWebHistory(),
